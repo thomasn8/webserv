f31097cd8bedaca064dac2503c0eb8f108e00f2d
Thomas Nanchen <thomasnanchen@hotmail.com>
2022-12-21 18:00:27 +0100

    parsing termine, commence socket-programming

--- thomas/1.socket-programing/socket_loop.cpp
+++ thomas/1.socket-programing/socket_loop.cpp
@@ -14,54 +14,101 @@
 #include <cstdlib>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#define PORT 8080
-#define LISTEN_BACKLOG 50
+#include <arpa/inet.h>
+
+#define SERVERS_COUNT 2
+#define PORT_1 80
+#define PORT_2 8080
+#define MAX_PENDING_CONNECTIONS 30
+#define MAX_CLIENT_SOCKET 30
+#define TRUE 1
+#define FALSE 0
+
+void exitWithError(std::string str, int code)
+{
+	perror(str.c_str()); 
+	exit(code); 
+}
 
 int main()
 {
-	int server_fd, client_fd; 
+	int master_socket[SERVERS_COUNT] = {0}, client_socket[MAX_CLIENT_SOCKET] = {0}, new_socket;
+	int ports[SERVERS_COUNT] = {PORT_1, PORT_2};
+	int opt = TRUE;
+    struct sockaddr_in addresses[SERVERS_COUNT];
+
+	for (int i = 0; i < SERVERS_COUNT; i++)
+	{
+		// crée un point d'entrée pour la communication à venir
+		// paramètre le socket pour permettre plusieurs connexion simultannée,
+		master_socket[i] = socket(AF_INET, SOCK_STREAM, 0);
+		if (master_socket[i] < 0)
+			exitWithError("error: master socket failed", 1);
+		if (setsockopt(master_socket[i], SOL_SOCKET, SO_REUSEADDR, (void *)&opt, sizeof(opt)) < 0)
+			exitWithError("error: multiple connections failed", 1);
+		
+		// attribution d'une struct pour contenir notamment l'adresse ip et le port
+		memset(addresses[i].sin_zero, 0, sizeof(addresses[i].sin_zero));
+		addresses[i].sin_family = AF_INET;
+		addresses[i].sin_addr.s_addr = htonl(INADDR_ANY);
+		addresses[i].sin_port = htons(ports[i]);
+		addresses[i].sin_len = sizeof(addresses[i]);
+
+		std::cout << ntohl(addresses[i].sin_addr.s_addr) << ":" << ntohs(addresses[i].sin_port);
+		std::cout << " listening on socket fd: " << master_socket[i] << std::endl;
+		
+		// affiliation (bind) de cette struct au fd généré par socket() pour les connexions/communications à venir
+		if (bind(master_socket[i], (struct sockaddr *) &addresses[i], sizeof(addresses[i])) < 0)
+			exitWithError("error: bind failed", 1);
+		// prépare notre socket à accepter des connexions
+		if (listen(master_socket[i], MAX_PENDING_CONNECTIONS) < 0)
+			exitWithError("error: listen failed", 1);
+	}
+
+	// creer un set de filedescriptors pour gerer des connexions sur plusieurs servers/ports
+	fd_set readfds;
+	int max_fd = -1;
 	long client_read;
-    struct sockaddr_in address;
 	char buffer[30000] = {0};
     std::string response = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 25\n\nHello from the server!";
-
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd < 0)
-	{
-		perror("socket failed"); 
-		exit(EXIT_FAILURE); 
-	}
-	memset(address.sin_zero, 0, sizeof(address.sin_zero));
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = htonl(INADDR_ANY);
-	address.sin_port = htons(PORT);
-	address.sin_len = sizeof(address);
-	if (bind(server_fd, (struct sockaddr *) &address, sizeof(address)) < 0)
-	{
-		perror("bind failed");
-		exit(EXIT_FAILURE);
-	}
-	if (listen(server_fd, LISTEN_BACKLOG) < 0)
-	{
-		perror("listen failed");
-		exit(EXIT_FAILURE);
-	}
 	while(1)
 	{
-		std::cout << std::endl << "+++++++ Waiting for new connection ++++++++" << std::endl << std::endl;
-		client_fd = accept(server_fd, (struct sockaddr *) &address, (socklen_t *) &address.sin_len);
-		if (client_fd < 0)
+		// clear the socket set
+		FD_ZERO(&readfds);
+		for (int i = 0; i < SERVERS_COUNT; i++)
 		{
-			perror("accept failed");
-			exit(EXIT_FAILURE);
+			// set the maximum fd number for select()
+			if (master_socket[i] > max_fd)
+				max_fd = master_socket[i];
+			// pass file descriptors of each server in the select fd set
+			FD_SET(master_socket[i], &readfds);
 		}
-		client_read = read(client_fd, buffer, 30000);
-		if (client_read < 0)
-			perror("No bytes are there to read");
-		std::cout << buffer << std::endl;
-		write(client_fd, response.c_str(), response.length());
-		std::cout << "------- Hello message sent -------" << std::endl;
-		close(client_fd);
+		// Once you have an incoming connection, the select would unblock and replaces the given descriptor sets with subsets consisting of those descriptors that are ready for the requested operation
+		if (select(max_fd + 1 , &readfds , NULL , NULL , NULL) < 0)
+			std::cerr << "error: select failed" << std::endl;
+
+		for (int i = 0; i < SERVERS_COUNT; i++)
+		{
+			// If something happened on the master socket, then its an incoming connection
+			if (FD_ISSET(master_socket[i], &readfds))
+			{
+				// Then accept will extracts the first connection request on the queue of pending connections, creates a new socket with the same properties of socket, and allocates a new file descriptor for the socket
+				// The argument address is a result parameter that is filled in with the address of the connecting entity, as known to the communications layer.
+				if ((new_socket = accept(master_socket[i], (struct sockaddr *)&addresses[i], (socklen_t *)&addresses[i].sin_len)) < 0)
+					std::cerr << "error: accept failed" << std::endl;
+				else
+				{
+					std::cout << "New connection, socket fd is " << new_socket << ", client ip is :" << inet_ntoa(addresses[i].sin_addr) << ", client port is :" <<  ntohs(addresses[i].sin_port) << std::endl;
+					client_read = read(new_socket, buffer, 30000);
+					if (client_read < 0)
+						std::cerr << "error: read: no bytes received from client" << std::endl;
+					std::cout << buffer << std::endl;
+					write(new_socket, response.c_str(), response.length());
+					std::cout << "Welcome message sent successfully" << std::endl;
+					close(new_socket);
+				}
+			}
+		}
 	}
 	return 0;
 }
