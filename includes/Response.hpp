f2a935b9a730e9f31f1ca9bd8e1c3925f1d38445
Michele-Reymond <hello@michelereymond.ch>
2023-01-08 15:18:06 +0100

    Compilation bug ok

--- includes/Response.hpp
+++ includes/Response.hpp
@@ -4,22 +4,38 @@
 #include "Message.hpp"
 #include "Request.hpp"
 
+class Server;
+
 class Response : public Message {
 	public:
-		Response(Request &request);
-		Response(const Response &instance);
+		// Response(Request request, Server &server);
+		Response(Request request);
+		// Response(const Response &instance);
 		virtual ~Response();
+
+		std::string getMessage() const;
+		std::string getStatusCode() const;
+		std::string getReason() const;
+		std::string getVersion() const;
 
 		Response		&operator=(const Response &instance);
 
 	private:
 		Response();
 
-		Request		&_request;
-		std::string _rowMessage;
-		std::string _method;
-		std::string _target;
+		void		_response_get();
+		void		_response_post();
+		void		_response_delete();
+		void		_check_target(std::string target);
+		
+		Request		_request;
+		// Server		&_server;
+		std::string _finalMessage;
 		std::string _version;
+		std::string _statusCode;
+		std::string _reason;
+		bool		_isCGI;
+		bool		_targetFound;
 };
 
 #endif
